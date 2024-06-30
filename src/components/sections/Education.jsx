18a7e3ae9acffa83ddc6200f947c516706656384
LBD <lyanbasora@gmail.com>
2024-06-30 00:00:22 -0400

    Made small changes

--- src/components/sections/Education.jsx
+++ src/components/sections/Education.jsx
@@ -1,8 +1,19 @@
-
+import Add from '../buttons/Add.jsx'
+import Edit from '../buttons/Edit.jsx'
 
 function Education() {
     return (
-    <section className='card'>Education section</section>
+        <section className='card'>
+        <section className='btn-container'>
+        <Add />
+        <Edit />
+        </section>
+        <section className='content' id='info-content'>
+            College University<br />Bachelor of Science<br />City, Country
+        </section>
+        
+        
+    </section>
 );
 }
 
--- src/components/sections/Experience.jsx
+++ src/components/sections/Experience.jsx
@@ -1,8 +1,19 @@
-
+import Add from '../buttons/Add.jsx'
+import Edit from '../buttons/Edit.jsx'
 
 function Experience() {
     return (
-    <section className='card'>Experience section</section>
+        <section className='card'>
+        <section className='btn-container'>
+        <Add />
+        <Edit />
+        </section>
+        <section className='content' id='info-content'>
+            Workplace Name<br />Job Role Name<br />City, Country
+        </section>
+        
+        
+    </section>
 );
 }
 
